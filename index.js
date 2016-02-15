cba8c638bed5fc1ede98bae3898d3b905f2a5ff1
NumminorihSF <numminorihsf@gmail.com>
2016-02-15 10:09:10 +0700

    change some lines order, add fallback formatter

--- index.js
+++ index.js
@@ -3,18 +3,25 @@
  */
 function getFormatter (options){
   options = options || {};
-  options.dataSource = options.dataSource || 'res.locals';
+
   var human = 'human' in options ? !!options.human : (process.env.NODE_ENV !== 'production');
-  options.formats = options.formats || ['application/json'];//TODO 'text/xml','text/plain', 'text/html'
   var formatter = {};
+
+  options.dataSource = options.dataSource || 'res.locals';
+  options.formats = options.formats || ['application/json'];//TODO 'text/xml','text/plain', 'text/html'
   options.formats.forEach(function(format){
+    //TODO user formatters
     if (human) formatter[format] = require('./human-formatter/'+format);
     else formatter[format] = require('./formatter/'+format);
   });
+
+  options.fallback = options.formats[0];
+
   return function(req, res, next){
 
   }
 }
+
 
 module.exports = getFormatter;
 
