bd8b859a9b36b0606ac90e98f3c3f2ac2fb741de
NumminorihSF <numminorihsf@gmail.com>
2016-02-15 09:53:46 +0700

    add index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+/**
+ * Created by numminorihsf on 15.02.16.
+ */
+function getFormatters (options){
+  options = options || {};
+  options.dataSource = options.dataSource || 'res.locals';
+  var human = 'human' in options ? !!options.human : (process.env.NODE_ENV !== 'production');
+  options.formats = options.formats || ['application/json'];//TODO 'text/xml','text/plain', 'text/html'
+  var formatter = {};
+  options.formats.forEach(function(format){
+    if (human) formatter[format] = require('./human-formatter/'+format);
+    else formatter[format] = require('./formatter/'+format);
+  });
+  return function(req, res, next){
+
+  }
+}
+
+module.exports = getFormatters();
+
