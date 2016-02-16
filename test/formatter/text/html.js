6f5cf24d5a09b957efa8c474be522dd50c71759e
NumminorihSF <numminorihsf@gmail.com>
2016-02-16 10:06:54 +0700

    fix test describe add many tests cases to object and fix tests for normalize input

--- test/formatter/text/html.js
+++ test/formatter/text/html.js
@@ -4,8 +4,11 @@
 var expect = require('chai').expect;
 var mod = require('../../../formatter/text/html');
 
+function A(){}
+A.prototype.toString = function(){return 'ASDFjkvcren,rt 12348gvf4'};
+
 describe('formatter/text/plain', function(){
-  it('return String as is', function(done){
+  it('return String wrapped to <span/>', function(done){
     var a = 'asdfe sdfjkejreGERT';
     mod(a, function(err, res){
       expect(res).to.be.equal('<span>asdfe sdfjkejreGERT</span>');
@@ -13,7 +16,7 @@ describe('formatter/text/plain', function(){
     });
   });
 
-  it('wrap Number to String', function(done){
+  it('wrap Number to <code/>', function(done){
     var a = 100500;
     mod(a, function(err, res){
       expect(res).to.be.equal('<code>100500</code>');
@@ -21,7 +24,7 @@ describe('formatter/text/plain', function(){
     });
   });
 
-  it('wrap NaN to String ("NaN")', function(done){
+  it('wrap NaN to <code>NaN</code>)', function(done){
     var a = NaN;
     mod(a, function(err, res){
       expect(res).to.be.equal('<code>NaN</code>');
@@ -29,7 +32,7 @@ describe('formatter/text/plain', function(){
     });
   });
 
-  it('wrap Boolean to String', function(done){
+  it('wrap Boolean to <mark/>', function(done){
     var a = true;
     mod(a, function(err, res){
       expect(res).to.be.equal('<mark>true</mark>');
@@ -37,22 +40,31 @@ describe('formatter/text/plain', function(){
     });
   });
 
-  it('wrap Object to formatted String', function(done){
+  it('wrap Object to <ul/>', function(done){
     var a = {
       1:2,
       3:"4",
-      5:false
+      5:false,
+      6:function(){},
+      7:undefined,
+      8:new A(),
+      9: null
     };
     mod(a, function(err, res){
       expect(res).to.be.equal('<ul>' +
         '<li>1:<code>2</code></li>' +
         '<li>3:<span>4</span></li>' +
-        '<li>5:<mark>false</mark></li></ul>');
+        '<li>5:<mark>false</mark></li>' +
+        '<li>6:</li>' +
+        '<li>7:</li>' +
+        '<li>8:<pre>ASDFjkvcren,rt 12348gvf4</pre></li>' +
+        '<li>9:<strong>null</strong></li>' +
+        '</ul>');
       done();
     });
   });
 
-  it('wrap deep Object to formatted String', function(done){
+  it('wrap deep Object to <ul/>', function(done){
     var a = {
       1:2,
       3:4,
@@ -70,32 +82,37 @@ describe('formatter/text/plain', function(){
     };
     mod(a, function(err, res){
       expect(res).to.be.equal('<ul>' +
-        '<li>1:2</li>' +
-        '<li>3:4</li>' +
-        '<li>5:6</li>' +
+        '<li>1:<code>2</code></li>' +
+        '<li>3:<code>4</code></li>' +
+        '<li>5:<code>6</code></li>' +
         '<li>a:<ul>' +
-        '<li>1:2</li>' +
-        '<li>2:3</li>' +
+        '<li>1:<code>2</code></li>' +
+        '<li>2:<code>3</code></li>' +
         '<li>3:<ul>' +
         '<li>4:<ul>' +
-        '<li>5:6</li>' +
+        '<li>5:<code>6</code></li>' +
         '</ul></li>' +
         '</ul></li>' +
-        '<li>5:3</li>' +
+        '<li>5:<code>3</code></li>' +
         '</ul></li>' +
         '</ul>');
       done();
     });
   });
 
-  it('wrap Array to formatted String', function(done){
+  it('wrap Array to <ol/>', function(done){
+    function A(){}
+    A.prototype.toString = function(){return 'ASDFjkvcren,rt 12348gvf4'};
     var a = [
       1,
       "2",
       true,
       4,
-      5,
-      6
+      undefined,
+      new A(),
+      function(){},
+      6,
+      null
     ];
     mod(a, function(err, res){
       expect(res).to.be.equal('<ol>'+
@@ -103,14 +120,17 @@ describe('formatter/text/plain', function(){
         '<li><span>2</span></li>' +
         '<li><mark>true</mark></li>' +
         '<li><code>4</code></li>' +
-        '<li><code>5</code></li>' +
+        '<li></li>' +
+        '<li><pre>ASDFjkvcren,rt 12348gvf4</pre></li>' +
+        '<li></li>' +
         '<li><code>6</code></li>' +
+        '<li><strong>null</strong></li>' +
         '</ol>');
       done();
     });
   });
 
-  it('wrap deep Array to formatted String', function(done){
+  it('wrap deep Array to <ol/>', function(done){
     var a = [
       1,
       [
@@ -130,30 +150,30 @@ describe('formatter/text/plain', function(){
     ];
     mod(a, function(err, res){
       expect(res).to.be.equal('<ol>' +
-        '<li>1</li>' +
+        '<li><code>1</code></li>' +
         '<li><ol>' +
-        '<li>2</li>' +
+        '<li><code>2</code></li>' +
         '<li><ol>' +
-        '<li>3</li>' +
+        '<li><code>3</code></li>' +
         '<li><ol>' +
-        '<li>4</li>' +
-        '<li>4</li>' +
-        '<li>4</li>' +
+        '<li><code>4</code></li>' +
+        '<li><code>4</code></li>' +
+        '<li><code>4</code></li>' +
         '</ol></li>' +
-        '<li>5</li>' +
+        '<li><code>5</code></li>' +
         '</ol></li>' +
-        '<li>6</li>' +
+        '<li><code>6</code></li>' +
         '</ol></li>' +
-        '<li>7</li>' +
+        '<li><code>7</code></li>' +
         '</ol>');
       done();
     });
   });
 
-  it('wrap null to JSON', function(done){
+  it('wrap null to formatted String', function(done){
     var a = null;
     mod(a, function(err, res){
-      expect(res).to.be.equal('null');
+      expect(res).to.be.equal('<strong>null</strong>');
       done();
     });
   });
@@ -161,7 +181,7 @@ describe('formatter/text/plain', function(){
   it('pass empty string to callback on Function', function(done){
     var a = function(){};
     mod(a, function(err, res){
-      expect(res).to.be.equal('<ul><li></li></ul>');
+      expect(res).to.be.equal('');
       done();
     });
   });
